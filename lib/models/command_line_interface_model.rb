0ff4e070f81954d25039a01d62864c3559925841
Jennifer Volpe <jennifervolpe@Jennifers-MacBook-Pro.local>
2017-08-01 18:01:15 -0400

    this is not working

--- lib/models/command_line_interface_model.rb
+++ lib/models/command_line_interface_model.rb
@@ -1,9 +1,11 @@
 require 'pry'
 class CommandLineInterfaceModel
 
+
   def greet
     puts "Welcome to the MovieActorCharacter Database, the command line solution for your Movie, Actor and character finding needs! Can't remember what movie that character is from? Want to prove to your friends that you were totally right about what movie THAT actor was in? Did that actor REALLY play that character? Find out here!"
   end
+
 
   def gets_user_input
     puts "Please enter the name of an Actor, character, or movie:"
@@ -38,8 +40,9 @@ class CommandLineInterfaceModel
     movie = Movie.find_by(title: title)
     if movie == nil
       try_again_movie
-    elsif
-      puts movie.title
+    else
+      puts "#{movie.title} is in our database! Would you like to see a cast list for #{movie.title}?"
+      get_cast_list_from_movie(title)
   end
 end
 
@@ -87,11 +90,37 @@ end
     end
   end
 
+  def get_cast_list_from_movie(movie)
+    puts "Please Enter Yes or No:"
+    cast_input = gets.chomp
+    if cast_input == "Yes"
+      puts "Here's the cast list for #{movie}: "
+      find_all_characters_from_movie(movie)
+      # puts "Would you like to see a list of #{movie.title}' characters?"
+      # gets_user_input_characters(actor)
+    elsif cast_input == "No"
+      puts "Ok. Anything else we can help you with?"
+      exit_or_continue
+    else
+      get_cast_list_from_movie(movie)
+      # puts find_movies(self)
+    end
+  end
+
+  def find_all_characters_from_movie(movie)
+    movie.characters.each do |character|
+      puts character.name
+    end
+    binding.pry
+  end
+
   def find_all_movies_from_actor(actor)
     actor.movies.each do |movie|
       puts movie.title
     end
   end
+
+  # def find_all
 
 
   def gets_user_input_characters(actor)
@@ -131,11 +160,16 @@ end
 
   def run
     greet
-    # input = gets_user_input.split(" ")
-    # find_actor(input[0], input[1])
     input = gets_user_input
+    title = Movie.first.title
+    charname = Character.first.name
+    actorname = Actor.first.name
+ if input == actorname
     find_actor(input)
-    # search(term, first_name, last_name, name, title)
+  elsif input== title
+    find_movies(input)
+  elsif input == charname
+    find_characters(input)
+    end
   end
-
 end
